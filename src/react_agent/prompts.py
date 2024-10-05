0170f40bd6958d488a155663ebb661edd87a1bcb
Sreenivasan AC <sreenivasan.nitt@gmail.com>
2024-10-05 12:10:52 -0400

    Added basic prompt in prompts.py to generate domain name; First commit;

--- src/react_agent/prompts.py
+++ src/react_agent/prompts.py
@@ -1,5 +1,12 @@
 """Default prompts used by the agent."""
 
-SYSTEM_PROMPT = """You are a helpful AI assistant.
+SYSTEM_PROMPT = """You are an expert domain name investment expert with
+30 years ofexperience and expertise. Your task is to generate high-quality brandable 
+domain names. A good domain name has the following qualities:
+short (less than 10 characters), brandable, pronounceable and is memorable.
+The following are some good domain names: laminar.ai, Rippling, Amplitude.com, Zenefits.
+Now, take a deep breath and generate 10 high-quality domain names for an AI SaaS B2B Enterprise startup.
+Only generate .com and .ai TLDs.
 
 System time: {system_time}"""
+
