9104e34885e31751bcba8af0d38e464f93df3773
George Roberts <pha10gr@gmail.com>
2025-02-26 16:10:40 +0100

    Make staticmethods

--- tw_ai_agents/agents/zendesk_agent_tools.py
+++ tw_ai_agents/agents/zendesk_agent_tools.py
@@ -11,26 +11,6 @@ from tw_ai_agents.react_agent.state import State
 llm = ChatOpenAI(model="gpt-4o")
 
 
-@tool
-def get_ticket_info(ticket_id: str):
-    """
-    Tool to get the ticket info from a zendesk ticket
-    :params:
-        ticket_id: The ID of the ticket to get information for
-    :return: The ticket information
-    """
-    return f"This is the ticket {ticket_id}. Infos: very nice ticket"
-
-
-@tool
-def get_comments(ticket_id: str):
-    """
-    Tool to get the comments from a zendesk ticket
-    :params:
-        ticket_id: The ID of the ticket to get comments for
-    :return: The comments from the ticket
-    """
-    return f"This is are the comments from the ticket {ticket_id}: [comment1, comment2, comment3]"
 
 
 class ZendeskAgentWithTools(BaseAgent):
@@ -41,11 +21,33 @@ class ZendeskAgentWithTools(BaseAgent):
 
     def get_graph(self):
 
-        tool_list = [get_comments, get_ticket_info]
+        tool_list = [self.get_comments, self.get_ticket_info]
         agent = create_react_agent(
             llm, tools=tool_list, prompt=self.system_prompt
         )
         return agent
+
+    @tool
+    @staticmethod
+    def get_ticket_info(ticket_id: str):
+        """
+        Tool to get the ticket info from a zendesk ticket
+        :params:
+            ticket_id: The ID of the ticket to get information for
+        :return: The ticket information
+        """
+        return f"This is the ticket {ticket_id}. Infos: very nice ticket"
+
+    @tool
+    @staticmethod
+    def get_comments(ticket_id: str):
+        """
+        Tool to get the comments from a zendesk ticket
+        :params:
+            ticket_id: The ID of the ticket to get comments for
+        :return: The comments from the ticket
+        """
+        return f"This is are the comments from the ticket {ticket_id}: [comment1, comment2, comment3]"
 
 
 if __name__ == "__main__":
