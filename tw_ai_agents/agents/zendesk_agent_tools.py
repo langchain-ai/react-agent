8c2ec692c304076fd3c361125efb24a2477f73c6
Federico Betti <federico.betti@outlook.com>
2025-02-26 16:11:18 +0100

    updated langgraph.json

--- tw_ai_agents/agents/zendesk_agent_tools.py
+++ tw_ai_agents/agents/zendesk_agent_tools.py
@@ -11,37 +11,37 @@ from tw_ai_agents.react_agent.state import State
 llm = ChatOpenAI(model="gpt-4o")
 
 
-@tool
-def get_ticket_info(ticket_id: str):
-    """
-    Tool to get the ticket info from a zendesk ticket
-    :params:
-        ticket_id: The ID of the ticket to get information for
-    :return: The ticket information
-    """
-    return f"This is the ticket {ticket_id}. Infos: very nice ticket"
-
-
-@tool
-def get_comments(ticket_id: str):
-    """
-    Tool to get the comments from a zendesk ticket
-    :params:
-        ticket_id: The ID of the ticket to get comments for
-    :return: The comments from the ticket
-    """
-    return f"This is are the comments from the ticket {ticket_id}: [comment1, comment2, comment3]"
-
-
 class ZendeskAgentWithTools(BaseAgent):
     def __init__(self, system_prompt: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.node_name = "ZendeskSearcher"
         self.system_prompt = system_prompt
 
+    @tool
+    @staticmethod
+    def get_ticket_info(ticket_id: str):
+        """
+        Tool to get the ticket info from a zendesk ticket
+        :params:
+            ticket_id: The ID of the ticket to get information for
+        :return: The ticket information
+        """
+        return f"This is the ticket {ticket_id}. Infos: very nice ticket"
+
+    @tool
+    @staticmethod
+    def get_comments(ticket_id: str):
+        """
+        Tool to get the comments from a zendesk ticket
+        :params:
+            ticket_id: The ID of the ticket to get comments for
+        :return: The comments from the ticket
+        """
+        return f"This is are the comments from the ticket {ticket_id}: [comment1, comment2, comment3]"
+
     def get_graph(self):
 
-        tool_list = [get_comments, get_ticket_info]
+        tool_list = [self.get_comments, self.get_ticket_info]
         agent = create_react_agent(
             llm, tools=tool_list, prompt=self.system_prompt
         )
