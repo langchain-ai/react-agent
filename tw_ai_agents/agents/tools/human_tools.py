42a9b36ea9d5501c0e6f4ca3ecc9628dfb6d1e6a
Federico Betti <federico.betti@outlook.com>
2025-03-05 12:11:51 +0100

    WIP

--- tw_ai_agents/agents/tools/human_tools.py
+++ tw_ai_agents/agents/tools/human_tools.py
@@ -6,8 +6,8 @@ COMPLETE_HANDOFF_STRING = "Handoff the full conversation to a real agent."
 
 @tool("real_human_agent_execute_actions")
 def real_human_agent_execute_actions(query: str) -> str:
-    """Make a real agent execute actions.
-
+    """
+    Make a real agent execute actions.
     This tool makes a real human agent execute actions based on the provided query.
 
     Args:
@@ -16,8 +16,6 @@ def real_human_agent_execute_actions(query: str) -> str:
     Returns:
         A string containing the information from the real human agent. It can be a confirmation or a negative answer.
     """
-    a = 1
-
     answer = interrupt(
         # This value will be sent to the client
         # as part of the interrupt information.
