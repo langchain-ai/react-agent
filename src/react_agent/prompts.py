8bc00fe30f56c43fdbea58baf3efb33be98ca13f
demarcus.freeman <demarcus.freeman@everbridge.com>
2025-03-11 09:25:57 -0500

    updating prompts with examples

--- src/react_agent/prompts.py
+++ src/react_agent/prompts.py
@@ -25,11 +25,18 @@ Follow this template EXACTLY:
 **User Agent:** [Extract from authorizerLogs, field "@message", find 'user-agent': ']
 
 ### 3. Log Information
+IMPORTANT: DO NOT ENCODE FORWARD SLASHES TO BE %2F, USE $252F FOR THIS SECTION
+
 **Authorizer Log Group:** [Extract from authorizerLogs, field "logGroup"]
 [Direct Log Stream]([Construct the URL: https://console.aws.amazon.com/cloudwatch/home?region=us-west-2#logsV2:log-groups/log-group/ +  (URL encode authorizerLogs.logGroup) + /log-events/ + (Extract from authorizerLogs, field "@logStream", URL encode this value)])
+EXAMPLE: https://us-west-2.console.aws.amazon.com/cloudwatch/home?region=us-west-2#logsV2:log-groups/log-group/$252Faws$252Flambda$252FAPI_Authorizer_us-west-2_dev/log-events/2025$252F03$252F10$252F%5B%24LATEST%5Dd32ce1a4746d445aaa3efc557958e112
+NOTE: Be sure this section follow this exact format: $252Faws$252Flambda$252F.  
+IMPORTANT: MAKE SURE TO URLENCODE THE LOGSTREAM LIKE THIS: 2025$252F03$252F10$252F%5B%24LATEST%5Dd32ce1a4746d445aaa3efc557958e112
 
 **Gateway Log Group:** [Extract from gatewayLogs, field "logGroup"]
-[Direct Log Search]([Construct the URL: https://console.aws.amazon.com/cloudwatch/home?region=us-west-2#logsV2:log-groups/log-group/ +  (URL encode gatewayLogs.logGroup) + $3FlogStreamNameFilter$3D + (Extract Extended Request ID from gatewayLogs, field "@message")])
+[Direct Log Search]([Construct the URL: https://console.aws.amazon.com/cloudwatch/home?region=us-west-2#logsV2:log-groups/log-group/ +  (URL encode gatewayLogs.logGroup) + /log-events/ + (Extract from gatewayLogs, field "@logStream", URL encode this value)])
+EXAMPLE: https://us-west-2.console.aws.amazon.com/cloudwatch/home?region=us-west-2#logsV2:log-groups/log-group/API-Gateway-Execution-Logs_3t80gsp5gc$252Fdev/log-events/ebc477c4c0089b2abe67b4d45beb5ffc
+
 
 ### 4. Request Outcome
 The logs don't explicitly show success/failure status. Next steps:
@@ -44,3 +51,7 @@ The logs don't explicitly show success/failure status. Next steps:
 You are provided with two JSON objects: `authorizerLogs` and `gatewayLogs`. Extract the necessary information from these objects to populate the report. Pay close attention to the extraction instructions and URL construction. If a field is not found, leave that field blank.
 
 System time: {system_time}"""
+
+
+https://us-west-2.console.aws.amazon.com/cloudwatch/home?region=us-west-2#logsV2:log-groups/log-group/API-Gateway-Execution-Logs_3t80gsp5gc%2Fdev?$3FlogStreamNameFilter$3D(extendedRequestId%3D(aafef9a7-0381-4a5e-a668-000c0df52c23)
+https://us-west-2.console.aws.amazon.com/cloudwatch/home?region=us-west-2#logsV2:log-groups/log-group/API-Gateway-Execution-Logs_3t80gsp5gc$252Fdev/log-events/ebc477c4c0089b2abe67b4d45beb5ffc
