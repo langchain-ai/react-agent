e2e26fc98276e78952e3be0fd7e8a53023de1a65
Caspar Broekhuizen <caspar@langchain.dev>
2025-10-17 15:20:25 -0700

    fix: int test

--- tests/integration_tests/test_graph.py
+++ tests/integration_tests/test_graph.py
@@ -4,13 +4,14 @@ from langsmith import unit
 from react_agent import graph
 from react_agent.context import Context
 
+pytestmark = pytest.mark.anyio
 
-@pytest.mark.asyncio
-@unit
+
 async def test_react_agent_simple_passthrough() -> None:
     res = await graph.ainvoke(
         {"messages": [("user", "Who is the founder of LangChain?")]},  # type: ignore
         context=Context(system_prompt="You are a helpful AI assistant."),
     )
+    print(res)
 
     assert "harrison" in str(res["messages"][-1].content).lower()
